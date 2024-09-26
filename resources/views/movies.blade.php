314fade932b83e658cb9b6aed27ee2a5a6ad5bfc
Aissa Ouadah <aouadah2004@gmail.com>
2024-09-26 22:46:54 +0200

    Added a homepage and some pages about movies. Clicking on the name of a movie brings you to another page with only that movie.

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies Page</title>
+</head>
+<body>
+    <article>
+        <a href="/movies/movie1">Movie 1</a>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare nisi vitae risus pellentesque consectetur. Vivamus lacinia nec orci a porttitor. Sed hendrerit, massa sed ornare facilisis, lacus mauris sollicitudin lorem, et vulputate massa velit vitae sem. Pellentesque id feugiat lorem. Aenean tortor dolor, placerat vitae sapien eget, tempor sagittis massa. In tristique dictum quam at ullamcorper. Quisque feugiat ac dui a faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer vel mauris eget nisi fringilla dignissim. Duis malesuada tincidunt efficitur.
+        </p>
+    </article>
+
+    <article>
+        <a href="/movies/movie2">Movie 2</a>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare nisi vitae risus pellentesque consectetur. Vivamus lacinia nec orci a porttitor. Sed hendrerit, massa sed ornare facilisis, lacus mauris sollicitudin lorem, et vulputate massa velit vitae sem. Pellentesque id feugiat lorem. Aenean tortor dolor, placerat vitae sapien eget, tempor sagittis massa. In tristique dictum quam at ullamcorper. Quisque feugiat ac dui a faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer vel mauris eget nisi fringilla dignissim. Duis malesuada tincidunt efficitur.
+        </p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('movies');
 });
+
+Route::get('/movies/{movie}', function ($slug) {
+    $movie = file_get_contents(__DIR__ . "/../resources/movies/{$slug}.html");
+
+    return view('movie', [
+        'movie' => $movie
+    ]);
+});
+
+//Route::get('/movies', function () {
+//    return view('movies');
+//});
